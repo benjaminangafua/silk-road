a1fe4dd642a6fed1c494985d8c43d0d2cf6dec82
benjaminangafua1997 <benjaminarkutl2017@gmail.com>
2022-11-10 12:01:32 +0000

    Updating responsive header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,9 @@
-<template>
-        <div class="header">
+<template> 
+        <div class="header" id="head">
             <div class="logo"><a href="/">SILKROAD</a> </div>
-            <div class="nav">
+            <div class="toggle">
+                
+            <div id="navbar" class="nav">
                 <div class="top-nav">
                     <div class="home">
                         <div><router-link to="/">Home</router-link> </div>
@@ -21,13 +23,57 @@
                     <div class="prty-btn"><button>Post a Property</button></div>
                 </div>
             </div>
+
+            <a  href="javascript:void(0);" class="icon" id="tgg">
+                <i class="fa fa-bars"></i>
+              </a>
+
+            </div>
         </div>
 </template>
 
-<script lang="ts">
+<script>
     export default {
         name: "Header",
-        
+        mounted(){
+
+    let head = document.getElementById("head")
+    let nav = document.getElementById("navbar")
+    let btn = document.getElementById("tgg")
+
+    let topNav = document.querySelector(".top-nav")
+    let hm = document.querySelector(".home")
+    let aut = document.querySelector(".auth")
+    let btm = document.querySelector(".bottom-nav")
+    let hr = document.querySelector("hr")
+    let prty = document.querySelector(".prty-btn")
+
+    // let xz = "none"
+    btn.onclick =()=>{
+        if(nav.className =="nav"){
+            nav.className = "response"
+            head.style = `display:block; background-color:#444`
+            topNav.style.display = "block"
+            hm.firstElementChild.style = `display:block;
+                                            margin: 10px 0px`
+            topNav.firstElementChild.style = `display:block;
+                                            margin: 8px 0px`
+            aut.style.display = "block"
+            aut.firstElementChild.style = `padding:0; margin-bottom: 10px;`
+            aut.lastElementChild.style = "padding:0"
+                                            
+            btm.style.display = "block"
+            prty.style = `width: max-content; margin-top: 10px`
+            hr.style.display = "none"
+                    
+        }else{
+            nav.className = "nav"
+            head.style = `display:grid; background-color:auto`
+
+    }
+}
+
+}
     }
 </script>
 
